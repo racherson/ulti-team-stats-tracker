a051d87348aed1d7f9e809dc56d74aa855486fdd
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2020-04-09 14:25:38 -0500

    set up coordinator with dummy views

--- StatsTracker/ViewController.swift
+++ StatsTracker/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController, Storyboarded {
+    
+    //MARK: Properties
+    weak var coordinator: RootCoordinator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: Actions
+    @IBAction func createAccount(_ sender: UIButton) {
+        coordinator?.createAccount()
+    }
+    
+    @IBAction func buyTapped(_ sender: UIButton) {
+        coordinator?.buySubscription()
+    }
 }
 
